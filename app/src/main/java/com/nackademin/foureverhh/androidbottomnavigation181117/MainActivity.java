491a611cc89e41814b8e49ff7f8f8cfb09786348
foureverhh <foureverhh@gmail.com>
2018-11-17 22:26:46 +0100

    Added disign library and nav_items menu and BottomNavigationView component.

--- app/src/main/java/com/nackademin/foureverhh/androidbottomnavigation181117/MainActivity.java
+++ app/src/main/java/com/nackademin/foureverhh/androidbottomnavigation181117/MainActivity.java
@@ -1,13 +1,16 @@
 package com.nackademin.foureverhh.androidbottomnavigation181117;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
     }
 }
