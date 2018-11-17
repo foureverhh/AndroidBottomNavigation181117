c3e27d28e02414c7062ada01c6ae949f8d4ac150
foureverhh <foureverhh@gmail.com>
2018-11-18 00:39:06 +0100

    Tested bottomNavigationView.setItemBackgroundResource to change color of bottom nav view

--- app/src/main/java/com/nackademin/foureverhh/androidbottomnavigation181117/MainActivity.java
+++ app/src/main/java/com/nackademin/foureverhh/androidbottomnavigation181117/MainActivity.java
@@ -1,8 +1,15 @@
 package com.nackademin.foureverhh.androidbottomnavigation181117;
 
+
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +19,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(nav);
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener nav =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment fragmentSelected = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_image:
+                            fragmentSelected = new ImageFragment();
+                            bottomNavigationView.setItemBackgroundResource(R.color.colorGreen);
+                            break;
+                        case R.id.nav_text:
+                            fragmentSelected = new WikipediaFragment();
+                            bottomNavigationView.setItemBackgroundResource(R.color.colorRed);
+                            break;
+                        case R.id.nav_history:
+                            fragmentSelected = new HistoryFragment();
+                            break;
+                    }
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container,fragmentSelected)
+                            .addToBackStack(null)
+                            .commit();
+
+                    return true;
+                }
+            };
 }
+
+
+
